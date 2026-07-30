9684ebfaf9f0bb29b3462b0b4152fae3a3251186
Sanit Kumar Amartya <sanit.connect01@gmail.com>
2026-07-30 13:11:30 +0530

    feat(core): establish logging foundation

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,8 @@
+const { logError } = require("../shared/logger");
+
 function errorHandler(err, req, res, next) {
+  logError(err.message || "Internal Server Error");
+
   res.status(err.status || 500).json({
     success: false,
     message: err.message || "Internal Server Error",
--- src/modules/core/controllers/index.js
+++ src/modules/core/controllers/index.js
@@ -1,10 +1,10 @@
 const { getHealthMessage } = require("../services");
-
 const { createSuccessResponse } = require("../../../shared/utils");
+const { logInfo } = require("../../../shared/logger");
 
 function healthController(req, res) {
+  logInfo("Health endpoint accessed");
   const message = getHealthMessage();
-
   res.json(createSuccessResponse(message));
 }
 
