15803718f7c30d4075a6c3f12d41881290a96aed
Sanit Kumar Amartya <sanit.connect01@gmail.com>
2026-08-01 15:28:36 +0530

    feat(api): add environment-based runtime configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   return res.status(200).json({
