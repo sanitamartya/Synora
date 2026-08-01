c66e122206ff72e5814379277d9916f66c0b6cef
Sanit Kumar Amartya <sanit.connect01@gmail.com>
2026-08-01 16:07:33 +0530

    refactor(api): introduce controller foundation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,8 @@
 import { Router } from "express";
+import { getHealth } from "../controllers/health.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.status(200).json({
-    success: true,
-    message: "Synora Backend Running",
-  });
-});
+router.get("/", getHealth);
 
 export default router;
