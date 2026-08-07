44d9c4fb9bd1037cc13e4cc27aae24d4a9d307ac
Sanit Kumar Amartya <sanit.connect01@gmail.com>
2026-08-07 14:14:10 +0530

    feat(shared): add asynchronous controller wrapper

---
+++ src/shared/asyncHandler.js
@@ -0,0 +1,9 @@
+export function asyncHandler(controller) {
+  return async function (req, res, next) {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
